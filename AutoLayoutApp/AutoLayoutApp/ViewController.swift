aa30b03844a4b0b5330320b58d4776da503eceaa
finsock <cgaspar@finsock.com>
2016-05-14 14:54:23 -0600

    AutoLayoutApp initial commit

---
+++ AutoLayoutApp/AutoLayoutApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AutoLayoutApp
+//
+//  Created by Csaba Gaspar on 2016-05-14.
+//  Copyright © 2016 Csaba Gaspar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
